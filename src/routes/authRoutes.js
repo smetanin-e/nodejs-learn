bf7f3bcfaba84d2bc045f6f8f3da3dc1c902caa9
smetanin-e <e91smet15@gmail.com>
2025-12-14 11:41:38 +0300

    add register route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,8 +5,33 @@ import db from '../db.js';
 
 const router = express.Router();
 
+// Register a new user endpoint /auth/register
 router.post('/register', (req, res) => {
-  console.log('register');
+  //const body = req.body;
+  const { username, password } = req.body;
+
+  const hashedPassword = bcrypt.hashSync(password, 8);
+
+  //save the new user and hashed password to the db
+  try {
+    const insertUser = db.prepare(`INSERT INTO users (username, password) VALUES (?, ?)`);
+    const result = insertUser.run(username, hashedPassword);
+
+    const defaultTodo = `Hello! Add your first todo!`;
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`);
+    insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+    //create a token
+    const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, {
+      expiresIn: '24h',
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error.message);
+    res.sendStatus(503);
+  }
+
+  res.sendStatus(201);
 });
 
 router.post('/login', (req, res) => {
